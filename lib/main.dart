7f16dca349e4d178d88eb7155faf9bc5b1d9f149
victordemolini <victor.demolini@sarria.salesians.cat>
2023-11-06 23:21:46 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello, World!"),
+      ),
+        body: Center(
+          child: Text("Víctor de Moliní Expósito 23/24"),
+        ),
+    ),
+  ));
+}
